272170cd2b9189ceb2ae9a5e8356e53ca67237b2
DarkLord0206 <tulsyan.parth0206@gmail.com>
2021-09-22 15:32:45 +0530

    Implemented vanilla Policy gradient

--- main.py
+++ main.py
@@ -1,60 +1,129 @@
+"""
+Simple policy gradient in Keras
+"""
+import gym
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.activations import sigmoid, softmax
-import gym
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import Adam
+import tensorflow.keras.utils as np_utils
+
+tf.compat.v1.disable_eager_execution()
 
 
-class A3C:
-    def __init__(self, state_size, action_size):
-        self.n_actions = action_size
-        self.lr = 0.01
-        self.memory_buffer = 2000
-        self.state_size = state_size
-        self.gamma = 0.5
-        self.probs = tf.convert_to_tensor([])
-        self.actions = tf.convert_to_tensor([])
-        self.rewards = tf.convert_to_tensor([])
-        self.log_probs = tf.convert_to_tensor([])
+class Agent(object):
+    def __init__(self, input_dim, output_dim):
+        # We will be making custom NN pipelines for policy gradient as we dont have a loss function which we have to
+        # minimize rather an objective function which we wnat to maximize
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.X = layers.Input(shape=(input_dim,))
+        net = self.X
+        net = layers.Dense(100)(net)
+        net = layers.Activation("relu")(net)
+        net = layers.Dense(50)(net)
+        net = layers.Activation("relu")(net)
+        net = layers.Dense(output_dim)(net)
+        net = layers.Activation("softmax")(net)
+        self.model = Model(inputs=self.X, outputs=net)
+        # this is our model whicih takes in self.X as input (having shape as (input_dims,)) and gives net (whose
+        # procedure is explained in above lines)
+        action_prob_placeholder = self.model.output
+        # the output is a softmax latyer of actions probability
 
-    def model_maker(self):
-        model = tf.keras.Sequetial(
-            Dense(units=100, input_dim=self.state_size, activation="relu"),
-            Dense(units=50, activation="relu"),
-            Dense(units=self.n_actions, activation=softmax)
-        )
+        # placeholders are variables (in form of tensors) which can be used in the computation graphs
 
-    def compute_action(self, model, state):
-        probs = model.predict(state)
-        action = np.random.choice(self.n_actions, p=probs)
-        self.probs.concat(probs[action])
-        self.log_probs.concat(probs[action])
-        self.actions.concat(action)
+        # action_onehot_placeholder is the variable to store which actions were taken in the trajectory and update them
+        # and use them to calculate grads to update NN
+        action_onehot_placeholder = K.placeholder(shape=(None, self.output_dim),
+                                                  name="action_onehot")
+        # dicount_reward_placeholder is the variable to store the discounted rewards which will be used to calulate
+        # grads for the NN
+        discount_reward_placeholder = K.placeholder(shape=(None,),
+                                                    name="discount_reward")
+        # findinng the probability of the action taken in that time step n the trajectory
+        action_prob = K.sum(action_prob_placeholder * action_onehot_placeholder, axis=1)
+        log_action_prob = K.log(action_prob)
+        # this is the function we have minimize (its actually maximize thats why the negative sign)
+        loss = - log_action_prob * discount_reward_placeholder
+        loss = K.mean(loss)
+        adam = Adam()
+        # defining the update process, params tells which all parameters to update , loss is the the loss function
+        # which is to be minimized
+        updates = adam.get_updates(params=self.model.trainable_weights,
+                                   loss=loss)
+        # definig the train function - take self.model.input as input and action_hot_placeholder,
+        # discount_onehot_placeholder as input and then call the Adam optimizer to calculte the loss from the
+        # procedure explained above and the output of the model which is to be trained is self.model.outputs get
+        # updates from updates variable above
+        self.train_fn = K.function(inputs=[self.model.input,
+                                           action_onehot_placeholder,
+                                           discount_reward_placeholder],
+                                   outputs=[self.model.outputs],
+                                   updates=updates)
 
-    def store_reward(self, reward):
-        self.rewards.concat(reward)
+    def get_action(self, state):
+        shape = state.shape
+        action_prob = np.squeeze(self.model.predict(state))
+        return np.random.choice(np.arange(self.output_dim), p=action_prob)
 
-    def discounted_rwds(self):
-        discounted_rewards = tf.convert_to_tensor([])
-        for x in range(self.rewards.shape[0]):
-            Gt = tf.Variable(0)
-            dsc = tf.Variable(0)
-            for y in self.rewards[x:]:
-                Gt = Gt + self.gamma ** dsc * y
-                dsc += 1
-            discounted_rewards.concat(Gt)
-        return discounted_rewards
-
-    def grads(self, rewards_dsc):
-        grads = rewards_dsc * self.log_probs
-        grads = grads.sum()
-        return grads
+    def fit(self, S, action_list, reward_list):
+        # action_list is the list of actions taken in the episode
+        # converting it to one hot vector
+        action_onehot = np_utils.to_categorical(action_list, num_classes=self.output_dim)
+        discount_reward = compute_discounted_R(reward_list)
+        # as defined above it takes in states,action_hot,discount_reward
+        self.train_fn([S, action_onehot, discount_reward])
 
 
-"""The Actor-Critic is basically like the brain of the A3C model. At it’s core it implements deep convolution Q learning
-, however the neural network now outputs two different items. The Actor and the Critic.
-The Critic measures how good the action taken is (value-based) V(s)
-The Actor outputs a set of action probabilities the agent can take (policy-based) Q(s,a)"""
-"""The Advantage is how the Critic tells the Actor that it’s predicted Q-values from the ANN are good or bad. It calculates the policy loss.
-This is calculated through the Advantage equation."""
+def compute_discounted_R(R, discount_rate=.99):
+    discounted_r = np.zeros_like(R, dtype=np.float32)
+    running_add = 0
+    for t in reversed(range(len(R))):
+        running_add = running_add * discount_rate + R[t]
+        discounted_r[t] = running_add
+    discounted_r -= (discounted_r.mean() / discounted_r.std())
+    return discounted_r
+
+
+def run_episode(env, agent):
+    done = False
+    state_list = []
+    action_list = []
+    reward_list = []
+    curr_state = env.reset()
+    total_reward = 0
+    while not done:
+        env.render()
+        action = agent.get_action(np.asarray([curr_state]))
+        next_state, reward, done, info = env.step(action)
+        total_reward += reward
+        state_list.append(curr_state)
+        action_list.append(action)
+        reward_list.append(reward)
+        curr_state = next_state
+
+        if done:
+            state_list = np.array(state_list)
+            action_list = np.array(action_list)
+            reward_list = np.array(reward_list)
+
+            agent.fit(state_list, action_list, reward_list)
+
+    return total_reward
+
+
+def main():
+    env = gym.make("Assault-ram-v0")
+    input_dim = env.observation_space.shape[0]
+    output_dim = env.action_space.n
+    agent = Agent(input_dim, output_dim)
+    for episode in range(2000):
+        reward = run_episode(env, agent)
+        print(episode, reward)
+
+
+if __name__ == '__main__':
+    main()
