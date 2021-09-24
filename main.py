0ea6bea83a8517e463ccb04734790b235704485d
DarkLord0206 <tulsyan.parth0206@gmail.com>
2021-09-24 14:05:28 +0530

    A2C Implemented.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
 from tensorflow.keras.optimizers import Adam
 import tensorflow.keras.utils as np_utils
+import matplotlib.pyplot as plt
 
 tf.compat.v1.disable_eager_execution()
 
@@ -19,18 +20,18 @@ class Agent(object):
         self.X = layers.Input(shape=(input_dim,))
 
         critic_net = self.X
-        critic_net = layers.Dense(100)(critic_net)
+        critic_net = layers.Dense(40)(critic_net)
         critic_net = layers.Activation("relu")(critic_net)
-        critic_net = layers.Dense(50)(critic_net)
+        critic_net = layers.Dense(10)(critic_net)
         critic_net = layers.Activation("relu")(critic_net)
         critic_net = layers.Dense(1)(critic_net)
 
         self.critic_model = Model(inputs=self.X, outputs=critic_net)
 
         actor_net = self.X
-        actor_net = layers.Dense(100)(actor_net)
+        actor_net = layers.Dense(40)(actor_net)
         actor_net = layers.Activation("relu")(actor_net)
-        actor_net = layers.Dense(50)(actor_net)
+        actor_net = layers.Dense(20)(actor_net)
         actor_net = layers.Activation("relu")(actor_net)
         actor_net = layers.Dense(output_dim)(actor_net)
         actor_net = layers.Activation("softmax")(actor_net)
@@ -38,6 +39,7 @@ class Agent(object):
         self.actor_model = Model(inputs=self.X, outputs=actor_net)
         # this is our model whicih takes in self.X as input (having shape as (input_dims,)) and gives net (whose
         # procedure is explained in above lines)
+        entropy_placeholder = K.placeholder(shape=(None,), name="entropy")
         action_prob_placeholder = self.actor_model.output
         value_placeholder = self.critic_model.output
         # the output is a softmax latyer of actions probability
@@ -59,12 +61,12 @@ class Agent(object):
         log_action_prob = K.log(action_prob)
         # this is the function we have minimize (its actually maximize thats why the negative sign)
         actor_loss = - log_action_prob * advantage_placeholder
-        actor_loss = K.mean(actor_loss)
-        adam = Adam()
+        actor_loss = K.mean(actor_loss) + 0.001 * entropy_placeholder
+        adam = Adam(learning_rate=0.01)
 
         critic_loss = advantage_placeholder ** 2 * 0.5
-        critic_loss = K.mean(critic_loss)
-        adam2 = Adam()
+        critic_loss = K.mean(critic_loss) + 0.001 * entropy_placeholder
+        adam2 = Adam(learning_rate=0.01)
         # defining the update process, params tells which all parameters to update , loss is the the loss function
         # which is to be minimized
         actor_updates = adam.get_updates(params=self.actor_model.trainable_weights, loss=actor_loss)
@@ -75,28 +77,30 @@ class Agent(object):
         # updates from updates variable above
         self.actor_train_fn = K.function(inputs=[self.actor_model.input,
                                                  action_onehot_placeholder,
-                                                 discount_reward_placeholder],
+                                                 discount_reward_placeholder, entropy_placeholder],
                                          outputs=[self.actor_model.outputs],
                                          updates=actor_updates)
-        self.critic_train_fn = K.function(inputs=[self.critic_model.input, discount_reward_placeholder],
-                                          outputs=[self.critic_model.outputs], updates=critic_updates)
+        self.critic_train_fn = K.function(
+            inputs=[self.critic_model.input, discount_reward_placeholder, entropy_placeholder],
+            outputs=[self.critic_model.outputs], updates=critic_updates)
 
     def get_action(self, state):
         shape = state.shape
         action_prob = np.squeeze(self.actor_model.predict(state))
         # print(action_prob)
         state_value = np.squeeze(self.critic_model.predict(state))
-        # print(action_prob)
-        return np.random.choice(np.arange(self.output_dim), p=action_prob), state_value
+        # print(state_value)
+        return np.random.choice(np.arange(self.output_dim), p=action_prob), -np.sum(
+            np.mean(action_prob) * np.log(action_prob + 1e-12))
 
-    def fit(self, state_list, action_list, reward_list, value_list):
+    def fit(self, state_list, action_list, reward_list, entropy):
         # action_list is the list of actions taken in the episode
         # converting it to one hot vector
         action_onehot = np_utils.to_categorical(action_list, num_classes=self.output_dim)
         discounted_r = compute_dicounted_R(reward_list)
         # as defined above it takes in states,action_hot,discount_reward
-        self.actor_train_fn([state_list, action_onehot, discounted_r])
-        self.critic_train_fn([state_list, discounted_r])
+        self.actor_train_fn([state_list, action_onehot, discounted_r, entropy])
+        self.critic_train_fn([state_list, discounted_r, entropy])
 
 
 def compute_dicounted_R(R, discount_rate=.99):
@@ -105,25 +109,26 @@ def compute_dicounted_R(R, discount_rate=.99):
     for t in reversed(range(len(R))):
         running_add = running_add * discount_rate + R[t]
         discounted_r[t] = running_add
-    discounted_r = (discounted_r - discounted_r.mean()+1e-12) / (discounted_r.std() + 1e-12)
+    discounted_r = (discounted_r - discounted_r.mean() + 1e-12) / (discounted_r.std() + 1e-12)
     return discounted_r
 
 
 def run_episode(env, agent):
+    entropy = 0
     done = False
     state_list = []
     action_list = []
     reward_list = []
-    value_list = []
     curr_state = env.reset()
     total_reward = 0
-    # print(agent.critic_model.weights)
     while not done:
-        env.render()
-        action, value = agent.get_action(np.asarray([curr_state]))
+        # print(agent.critic_model.layers[0].weights[0])
+        # env.render()
+        action, entropy_curr = agent.get_action(np.asarray([curr_state]))
+        entropy += entropy_curr
         next_state, reward, done, info = env.step(action)
         total_reward += reward
-        value_list.append(value)
+        # print(action,)
         state_list.append(curr_state)
         action_list.append(action)
         reward_list.append(reward)
@@ -133,9 +138,8 @@ def run_episode(env, agent):
             state_list = np.array(state_list)
             action_list = np.array(action_list)
             reward_list = np.array(reward_list)
-            value_list = np.array(value_list)
-            agent.fit(state_list, action_list, reward_list, value_list)
-    # print(agent.critic_model.weights)
+            if total_reward != 0:
+                agent.fit(state_list, action_list, reward_list, entropy)
     return total_reward
 
 
@@ -144,9 +148,14 @@ def main():
     input_dim = env.observation_space.shape[0]
     output_dim = env.action_space.n
     agent = Agent(input_dim, output_dim)
-    for episode in range(2000):
+    rewards = []
+    for episode in range(1000):
         reward = run_episode(env, agent)
+        rewards.append(reward)
         print(episode, reward)
+    env.close()
+    plt.plot(range(len(rewards)), rewards)
+    plt.savefig("A2C.png")
 
 
 if __name__ == '__main__':
